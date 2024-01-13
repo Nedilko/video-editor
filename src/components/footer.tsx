a96cbd2fe74716613ea0dcdd78afabb9c12d597c
onedilko <onedilko@Oleksandrs-MacBook-Pro.local>
2024-01-14 00:19:25 +0200

    add basic ui

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return <footer className="w-full border-t border-border/40 bg-gray-800/10">
+    <div className="flex justify-start items-center h-16 text-white ml-2">
+      <p>Created by <a href="#" className="underline">Oleksandr Nedilko</a></p>
+    </div>
+  </footer>;
+}
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,13 @@
+import { Footer } from "@components/footer";
+import { Header } from "@components/header";
 import { PropsWithChildren } from "react";
 
 export const Layout = ({ children }: PropsWithChildren) => {
-  return <div className="m-4">
-    {children}
+  return <div className="bg-background flex flex-col h-screen justify-between">
+    <Header />
+    <main className="m-2 mb-auto">
+      {children}
+    </main>
+    <Footer />
   </div>
 }
