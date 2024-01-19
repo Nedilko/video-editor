5c15a8e805e8559af12671c9c45fd2263c8b8472
onedilko <onedilko@Oleksandrs-MacBook-Pro.local>
2024-01-19 18:51:44 +0200

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,30 @@ import { useDisclosure } from "@hooks/useDisclosure";
 
 function App() {
   const [isSidebarOpened, { open: openSidebar, close: closeSidebar }] = useDisclosure(true)
-
-  return <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-    <Layout>
-      <ResizablePanelGroup
-        direction="horizontal"
-        autoSaveId="panels-position"
-        className="flex gap-2"
-      >
-        <ResizablePanel id="editor" defaultSize={80} className="border rounded-md">
-          <Editor isSidebarOpened={isSidebarOpened} onOpenSidebar={openSidebar}/>
-        </ResizablePanel>
-        {isSidebarOpened && (
-          <>
-            <ResizableHandle withHandle/>
-            <ResizablePanel id="sidebar" defaultSize={20} className="border rounded-md" minSize={20} maxSize={40}>
-              <Sidebar onClose={closeSidebar}/>
-            </ResizablePanel>
-          </>
-        )}
-      </ResizablePanelGroup>
-    </Layout>
-    <Toaster/>
-  </ThemeProvider>
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Layout>
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="panels-position"
+          className="flex gap-2"
+        >
+          <ResizablePanel order={1} id="editor" defaultSize={80} className="border rounded-md">
+            <Editor isSidebarOpened={isSidebarOpened} onOpenSidebar={openSidebar}/>
+          </ResizablePanel>
+          {isSidebarOpened && (
+            <>
+              <ResizableHandle withHandle/>
+              <ResizablePanel order={2} id="sidebar" defaultSize={20} className="border rounded-md" minSize={20} maxSize={40}>
+                <Sidebar onClose={closeSidebar}/>
+              </ResizablePanel>
+            </>
+          )}
+        </ResizablePanelGroup>
+      </Layout>
+      <Toaster/>
+    </ThemeProvider>
+  )
 }
 
 export default App
